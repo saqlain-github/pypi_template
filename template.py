ba3ec0291fd7abecca95509235bb5b6fe4a41ed9
saqlain-github <syedsaqlainahmed43@gmail.com>
2022-10-18 12:44:17 +0530

    deleted unwanted files

--- template.py
+++ template.py
@@ -17,18 +17,9 @@ logging.info(f"Creating Project by name : {project_name}")
 # list of files:
 
 list_of_files = [
-    ".github/workflows/.gitkeep",
+    
     f"src/{project_name}/__init__.py",
-    f"tests/__init__.py",
-    f"tests/unit/__init__.py",
-    f"tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "pyproject.toml",
-    "setup.cfg",
-    "tox.ini"
+    
 ]
 
 
